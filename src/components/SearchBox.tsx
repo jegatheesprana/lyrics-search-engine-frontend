c25d90f313e390561410b596cd8ed42465b578fe
jegatheesprana <jegatheesprana@gmail.com>
2023-01-19 04:21:42 +0530

    Autocomplete

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -5,16 +5,24 @@ import Paper from "@mui/material/Paper"
 import IconButton from "@mui/material/IconButton"
 import InputBase from "@mui/material/InputBase"
 import Divider from "@mui/material/Divider"
+import Box from "@mui/material/Box"
+import Collapse from "@mui/material/Collapse"
+import Autocomplete from "@mui/material/Autocomplete"
 
 import SearchIcon from "@mui/icons-material/Search"
 import FilterListIcon from "@mui/icons-material/FilterList"
 import MenuIcon from "@mui/icons-material/Menu"
+import CloseIcon from "@mui/icons-material/Close"
+
 import Filter from "./Filter"
 
 const SearchBox = () => {
     const [searchParams, setSearchParams] = useSearchParams()
     const [query, setQuery] = useState(searchParams.get("query") || "")
+    const [metaphor, setMetaphor] = useState(searchParams.get("metaphor") || "")
     const [FilterOpen, setFilterOpen] = useState(false)
+    const [showMetaphorInput, setShowMetaphorInput] = useState(false)
+    const [fillData, setFillData] = useState([])
 
     const handleClickOpen = () => {
         setFilterOpen(true)
@@ -24,20 +32,56 @@ const SearchBox = () => {
         setFilterOpen(false)
     }
 
-    const handleChange = (e: any) => {
-        setQuery(e.target.value)
+    const handleChange = (e: any, newValue: string) => {
+        setQuery(newValue)
+    }
+
+    const handleMetaphorChange = (e: any) => {
+        setMetaphor(e.target.value)
     }
 
     const handleSubmit = (e: any) => {
         e.preventDefault()
-        setSearchParams({ query })
+        if (metaphor) setSearchParams({ query, metaphor })
+        else setSearchParams({ query })
+    }
+
+    const openMetaphorInput = () => {
+        setShowMetaphorInput(true)
+    }
+
+    const closeMetaphorInput = () => {
+        setShowMetaphorInput(false)
     }
 
     useEffect(() => {
         if (!searchParams.get("query")) return
         // @ts-ignore
         setQuery(searchParams.get("query"))
+        // @ts-ignore
+        setMetaphor(searchParams.get("metaphor"))
     }, [searchParams])
+
+    useEffect(() => {
+        if (!query) return
+        const controller = new AbortController()
+        fetch("http://127.0.0.1:5000/autofill", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                query,
+            }),
+            signal: controller.signal,
+        })
+            .then((res) => res.json())
+            .then((data: any) => {
+                setFillData(
+                    data.results.map((data: any) => data._source.song_name)
+                )
+            })
+    }, [query])
 
     return (
         <>
@@ -46,37 +90,83 @@ const SearchBox = () => {
                 onSubmit={handleSubmit}
                 sx={{
                     p: "2px 4px",
-                    display: "flex",
-                    alignItems: "center",
                     width: "100%",
                 }}
             >
-                <IconButton sx={{ p: "10px" }} aria-label="menu">
-                    <MenuIcon />
-                </IconButton>
-                <InputBase
-                    sx={{ ml: 1, flex: 1 }}
-                    placeholder="Search Songs"
-                    inputProps={{ "aria-label": "search google maps" }}
-                    value={query}
-                    onChange={handleChange}
-                />
-                <IconButton
-                    type="submit"
-                    sx={{ p: "10px" }}
-                    aria-label="search"
+                <Box
+                    sx={{
+                        display: "flex",
+                        alignItems: "center",
+                    }}
                 >
-                    <SearchIcon />
-                </IconButton>
-                <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-                <IconButton
-                    color="primary"
-                    sx={{ p: "10px" }}
-                    aria-label="directions"
-                    onClick={handleClickOpen}
-                >
-                    <FilterListIcon />
-                </IconButton>
+                    <IconButton
+                        sx={{ p: "10px" }}
+                        aria-label="menu"
+                        onClick={openMetaphorInput}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Autocomplete
+                        fullWidth
+                        id="free-solo-demo"
+                        freeSolo
+                        // options={top100Films.map((option) => option.title)}
+                        options={fillData}
+                        inputValue={query}
+                        onInputChange={handleChange}
+                        renderInput={(params: any) => (
+                            <InputBase
+                                {...params}
+                                sx={{ ml: 1, flex: 1 }}
+                                placeholder="Search Songs"
+                                ref={params.InputProps.ref}
+                            />
+                        )}
+                    />
+
+                    <IconButton
+                        type="submit"
+                        sx={{ p: "10px" }}
+                        aria-label="search"
+                    >
+                        <SearchIcon />
+                    </IconButton>
+                    <Divider
+                        sx={{ height: 28, m: 0.5 }}
+                        orientation="vertical"
+                    />
+                    <IconButton
+                        color="primary"
+                        sx={{ p: "10px" }}
+                        aria-label="directions"
+                        onClick={handleClickOpen}
+                    >
+                        <FilterListIcon />
+                    </IconButton>
+                </Box>
+                <Collapse in={showMetaphorInput}>
+                    <Box
+                        sx={{
+                            display: "flex",
+                            alignItems: "center",
+                        }}
+                    >
+                        <IconButton
+                            sx={{ p: "10px" }}
+                            aria-label="menu"
+                            onClick={closeMetaphorInput}
+                        >
+                            <CloseIcon />
+                        </IconButton>
+                        <InputBase
+                            sx={{ ml: 1, flex: 1 }}
+                            placeholder="Search Metaphor"
+                            inputProps={{ "aria-label": "search metaphor" }}
+                            value={metaphor}
+                            onChange={handleMetaphorChange}
+                        />
+                    </Box>
+                </Collapse>
             </Paper>
             <Filter open={FilterOpen} handleClose={handleClose} />
         </>
--- src/components/SearchResultItem.tsx
+++ src/components/SearchResultItem.tsx
@@ -7,8 +7,17 @@ import CardContent from "@mui/material/CardContent"
 import Button from "@mui/material/Button"
 import Typography from "@mui/material/Typography"
 import Stack from "@mui/material/Stack"
+import Collapse from "@mui/material/Collapse"
 
-export default function SearchResultItem({ result: { _source: result } }: any) {
+export default function SearchResultItem({
+    result: { _source: result, _id },
+}: any) {
+    const [show, setShow] = useState(false)
+
+    const handleClickToggle = () => {
+        setShow(!show)
+    }
+
     return (
         <Card sx={{ minWidth: 275 }}>
             <CardContent>
@@ -19,7 +28,7 @@ export default function SearchResultItem({ result: { _source: result } }: any) {
                 >
                     {result.year}
                 </Typography>
-                <Link to={`/results/${result.id}`}>
+                <Link to={`/results/${_id}`}>
                     <Typography variant="h5" component="div">
                         {result.song_name}
                     </Typography>
@@ -27,44 +36,63 @@ export default function SearchResultItem({ result: { _source: result } }: any) {
                 <Typography sx={{ mb: 1.5 }} color="text.secondary">
                     {result.album}
                 </Typography>
-                <Typography variant="body2">{result.lyrics}</Typography>
 
-                <Typography variant="body1">
-                    <b>Year</b> {result.year}
+                <Collapse in={show}>
+                    <Box sx={{ mb: 1.5, textAlign: "center" }}>
+                        <Typography variant="body1">
+                            <b>Year</b> {result.year}
+                        </Typography>
+                        <Typography variant="body1">
+                            <b>Album</b> {result.album}
+                        </Typography>
+                        <Typography variant="body1">
+                            <b>Song Name</b> {result.song_name}
+                        </Typography>
+                        <Typography variant="body1">
+                            <b>Lyricist</b> {result.lyricist}
+                        </Typography>
+                        <Typography variant="body1">
+                            <b>Singers</b> {result.singers}
+                        </Typography>
+                        <Typography variant="body1">
+                            <b>Composer</b> {result.composer}
+                        </Typography>
+                    </Box>
+                </Collapse>
+
+                <Typography variant="body2">
+                    {show
+                        ? result.lyrics.split("\n").map((line: string) => (
+                              <>
+                                  {line}
+                                  <br />
+                              </>
+                          ))
+                        : result.lyrics.slice(0, 200) + "..."}
                 </Typography>
-                <Typography variant="body1">
-                    <b>Album</b> {result.album}
-                </Typography>
-                <Typography variant="body1">
-                    <b>Song Name</b> {result.song_name}
-                </Typography>
-                <Typography variant="body1">
-                    <b>Lyricist</b> {result.lyricist}
-                </Typography>
-                <Typography variant="body1">
-                    <b>Singers</b> {result.singers}
-                </Typography>
-                <Typography variant="body1">
-                    <b>Composer</b> {result.composer}
-                </Typography>
-                <Stack sx={{ mt: 2 }} spacing={2}>
-                    {result.metaphors.map((metaphor: any, id: any) => (
-                        <Card key={id}>
-                            <Typography variant="body1">
-                                <b>Metaphor</b> {metaphor.metaphor}
-                            </Typography>
-                            <Typography variant="body1">
-                                <b>Target Domain</b> {metaphor.target}
-                            </Typography>
-                            <Typography variant="body1">
-                                <b>Source Domain</b> {metaphor.source}
-                            </Typography>
-                        </Card>
-                    ))}
-                </Stack>
+
+                <Collapse in={show}>
+                    <Stack sx={{ mt: 2, mx: 4 }} spacing={2}>
+                        {result.metaphors.map((metaphor: any, id: any) => (
+                            <Card key={id} sx={{ p: 1 }}>
+                                <Typography variant="body2">
+                                    <b>Metaphor</b> {metaphor.metaphor}
+                                </Typography>
+                                <Typography variant="body2">
+                                    <b>Target Domain</b> {metaphor.target}
+                                </Typography>
+                                <Typography variant="body2">
+                                    <b>Source Domain</b> {metaphor.source}
+                                </Typography>
+                            </Card>
+                        ))}
+                    </Stack>
+                </Collapse>
             </CardContent>
             <CardActions>
-                <Button size="small">View</Button>
+                <Button size="small" onClick={handleClickToggle}>
+                    {show ? "Show Less" : "Show More"}
+                </Button>
             </CardActions>
         </Card>
     )
